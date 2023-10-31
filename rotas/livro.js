3a4c226ce844e608d7e3ea0841b21877d1e5c129
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2023-10-30 23:27:28 -0300

    padrão de de req delete put post e patch

--- rotas/livro.js
+++ rotas/livro.js
@@ -6,4 +6,15 @@ router.get('/', (req, res) => {
     res.send("Testando a rota default")
 })
 
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
+})
 module.exports = router
