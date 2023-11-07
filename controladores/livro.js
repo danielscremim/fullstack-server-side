b3b7b8e27946b9e5edcfaf394f19c716f2432c3d
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2023-11-06 23:17:10 -0300

    requisição com paramaetrização Exemplo utilizado foi a get por ID

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,9 +1,9 @@
-const { getTodosLivros } = require("../servicos/livro")
+const { getTodosLivros, getLivroPorId } = require("../servicos/livro")
 
 function getLivros(req, res) {
     try {
         //throw new Error("teste")
-        const livros = getTodosLivros()
+        const livros = getTodosLivros(id)
         res.send(livros)
     } catch (error) {
         res.status(500)
@@ -11,6 +11,19 @@ function getLivros(req, res) {
     }
 }
 
+function getLivro(req, res) {
+    try {
+        //throw new Error("teste")
+        const id = req.params.id
+        const livro = getLivroPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro
 }
