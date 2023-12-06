262cb0d2567dd715dc71f49f6858a53c55e97dfb
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2023-12-05 23:48:48 -0300

    faltando debuggar o patch

--- servicos/livro.js
+++ servicos/livro.js
@@ -16,12 +16,22 @@ function insereLivro(livroNovo) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
     
     const novaListaDeLivros = [ ...livros, livroNovo ]
-
     fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
 
+function modificaLivro(modificacoes, id) {
+    let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
+    const indiceModificado = livrosAtuais.findIndex(livros => livro.id === id)
+
+    const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
+
+    livrosAtuais[indiceModificado] = conteudoMudado
+
+    fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
+}
 module.exports = {
     getTodosLivros,
     getLivroPorId,
-    insereLivro
+    insereLivro,
+    modificaLivro
 }
